dd0fdb73f4db4344e5412054d1543378773861ae
David Carrillo <david.carrilloc@alumno.buap.mx>
2022-10-17 19:43:44 -0500

    New features

---
+++ encriptador.js
@@ -0,0 +1,11 @@
+			stringAEncriptar = "alura"
+			stringAEncriptar = stringAEncriptar.toLowerCase();
+			let matrizCodigo = [["e","enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+
+			for (let i = 0; i < matrizCodigo.length; i++) {
+				if (stringAEncriptar.includes(matrizCodigo[i][0])) {
+					stringAEncriptar = stringAEncriptar.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+				}
+			}
+
+			console.log(stringAEncriptar)
